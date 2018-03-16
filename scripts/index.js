0f0d1d827b68787cc53cf6e0156de0b9dbe89ffc
esdes-fleury-antoine <32122421+esdes-fleury-antoine@users.noreply.github.com>
2018-03-16 15:28:00 +0100

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -5,26 +5,20 @@ var lineDrawing = anime({
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
     duration: 1500,
-    delay: function(el, i) { return i * 250 },
+    delay: function(el, i) { return i * 0 },
     direction: 'alternate',
     loop: true
   });
 
-  /**TIMELINE//
-   * test timeine
-
-var timeline = anime.timeline();
+  var timeline = anime.timeline();
 timeline.add({
     targets: '#coming-soon',
     opacity: 1,
     delay: 1000
 });
+
 timeline.add({
     targets: '#coming-soon',
     translateY: -100
 
 });
-timeline.add({
-    targets: '#coming-soon',
-
-})
